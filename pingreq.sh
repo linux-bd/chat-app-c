a6c109de4b5d0a4175effb989b60e1c65458eb3d
amran <smamran09@gmail.com>
2016-04-16 11:24:18 +0000

    first commit

---
+++ pingreq.sh
@@ -0,0 +1,19 @@
+#
+#How to create small menu using dialog
+#
+dialog --backtitle "Linux Shell Script Tutorial " --title "Main\
+Menu" --menu "Move using [UP] [DOWN],[Enter] to\
+Select" 15 50 3\
+Date/time "Shows Date and Time"\
+Calendar "To see calendar "\
+Editor "To start vi editor " 2>/tmp/menuitem.$$
+
+menuitem=`cat /tmp/menuitem.$$`
+
+opt=$?
+
+case $menuitem in
+Date/time) date;;
+Calendar) cal;;
+Editor) vi;;
+esac
